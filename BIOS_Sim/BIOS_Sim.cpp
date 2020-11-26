d2e6223ca5555dee98c716745b12de826c733362
GendalfBlack <garmash_a@itstep.academy>
2020-11-26 09:59:21 +0200

    Serious improvement is structure. UI update & expand.

--- BIOS_Sim/BIOS_Sim.cpp
+++ BIOS_Sim/BIOS_Sim.cpp
@@ -2,8 +2,15 @@
 #include <windows.h>
 #include <conio.h>
 #include <string>
+#include <deque>
 using namespace std;
 
+enum Borders { LEFT_MAIN = 0,			RIGHT_MAIN = 91,			TOP_MAIN = 1,			BOTTOM_MAIN = 27,			RIGHT_SIDE = 129 };
+enum Main	 { LEFT_MAIN_IN = 1,		RIGHT_MAIN_IN = 90,			TOP_MAIN_IN = 2,		BOTTOM_MAIN_IN = 26 };
+enum Side    { LEFT_SIDE_IN = 92,		RIGHT_SIDE_IN = 128 };
+enum Controls{ TOP_CONTROLS_IN = 28,	BOTTOM_BOTTOM_IN = 30 };
+enum Colors  { BLUE_BG_WHITE_TEXT = 23, GREY_BG_BLACK_TEXT = 112 ,	GREY_BG_BLUE_TEXT = 121, CYAN_BG_BLACK_TEXT = 176,	CYAN_BG_WHITE_TEXT = 191};
+enum Keys	 { LEFT_ARROW = 75,			F1 = 59,					RIGHT_ARROW = 77};
 HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
 /// <summary>
@@ -59,9 +66,9 @@ public:
 	/// </summary>
 	/// <param name="indent">Desired indent from the top of the console screen.</param>
 	void Show(int indent) {
-		Goto(1, index + indent);
+		Goto(LEFT_MAIN_IN, index + indent);
 		cout << name;
-		Goto(40, index + indent);
+		Goto(LEFT_MAIN_IN + 40, index + indent);
 		cout << "default";
 	}
 };
@@ -80,106 +87,140 @@ public:
 	/// </summary>
 	bool selected = false;
 	/// <summary>
-	/// Length of the name of the Menu.
-	/// </summary>
-	int x = 0;
-	/// <summary>
 	/// Items list.
 	/// </summary>
-	Item* items;
-	/// <summary>
-	/// Length of the items list.
-	/// </summary>
-	int items_length;
+	deque<Item> items;
 
-	Menu() : name("default"), items(nullptr), items_length(0){}
+	Menu() : name("default"), items(deque<Item>()), selected(false){	}
 	Menu(string name) {
 		this->name = name;
-		this->x = name.length();
+		items = deque<Item>();
 	}
 
 	void Setup(int index) {
 		Goto(index, 0);
 		if (selected)
 		{
-			SetConsoleTextAttribute(h, 121);
+			SetConsoleTextAttribute(h, GREY_BG_BLUE_TEXT);
 		}
 		else {
-			SetConsoleTextAttribute(h, 23);
+			SetConsoleTextAttribute(h, BLUE_BG_WHITE_TEXT);
 		}
+		for (int i = 0; i < (15 - name.length()) / 2; i++) { cout << ' '; }
 		cout << name;
+		for (int i = 0; i < (15 - name.length()) / 2; i++) { cout << ' '; }
 		SetConsoleTextAttribute(h, 121);
 	}
 
 	void Show() {
-		Goto(1, 3);
-		SetConsoleTextAttribute(h, 121);
-		for (int i = 0; i < items_length; i++) {
-			items[i].Show(2);
+		SetConsoleTextAttribute(h, GREY_BG_BLUE_TEXT);
+		for (int i = 0; i < items.size(); i++) {
+			items.at(i).Show(2);
 		}
-		SetConsoleTextAttribute(h, 23);
+		SetConsoleTextAttribute(h, BLUE_BG_WHITE_TEXT);
 	}
+	operator bool() { return name.length(); }
 };
 
-void Clear() {
-	Goto(1, 2);
-	for (int i = 0; i < 15; i++)
+void ClearMain() {
+	Goto(LEFT_MAIN_IN, TOP_MAIN_IN);
+	for (int i = TOP_MAIN_IN; i < BOTTOM_MAIN_IN + TOP_MAIN_IN; i++)
 	{
-		for (int j = 0; j < 91; j++)
+		for (int j = 0; j < RIGHT_MAIN_IN; j++)
 		{
 			cout << ' ';
 		}
-		cout << '\n';
+		Goto(LEFT_MAIN_IN, i);
 	}
 }
-
-Menu * menues = new Menu[6];
-
-enum { LEFT_MAIN = 0, RIGHT_MAIN = 91, TOP_MAIN = 1, BOTTOM_MAIN = 27, RIGHT_SIDE = 128 };
-
+void ClearSide() {
+	Goto(LEFT_SIDE_IN, TOP_MAIN_IN);
+	for (int i = TOP_MAIN_IN; i < BOTTOM_MAIN_IN + TOP_MAIN_IN; i++)
+	{
+		for (int j = LEFT_SIDE_IN; j < RIGHT_SIDE_IN + 1; j++)
+		{
+			cout << ' ';
+		}
+		Goto(LEFT_SIDE_IN, i);
+	}
+}
+void ClearControls() {
+	SetConsoleTextAttribute(h, CYAN_BG_WHITE_TEXT);
+	Goto(LEFT_MAIN, TOP_CONTROLS_IN);
+	for (int i = TOP_CONTROLS_IN; i < BOTTOM_BOTTOM_IN + TOP_CONTROLS_IN; i++)
+	{
+		for (int j = LEFT_MAIN; j < RIGHT_SIDE + 1; j++)
+		{
+			cout << ' ';
+		}
+		Goto(LEFT_MAIN, i);
+	}
+}
 void DrawBG() {
-	SetConsoleTextAttribute(h, 23);
+	SetConsoleTextAttribute(h, BLUE_BG_WHITE_TEXT);
 	Goto(LEFT_MAIN, 0);
-	for (int i = LEFT_MAIN; i < 129; i++)
-	{
-		cout << ' ';
-	}
-	SetConsoleTextAttribute(h, 121);
-	Goto(TOP_MAIN, 2);
-	for (int i = LEFT_MAIN; i < RIGHT_SIDE * BOTTOM_MAIN; i++)
-	{
-		cout << ' ';
-	}
+	for (int i = LEFT_MAIN; i < RIGHT_SIDE_IN + 2; i++){ cout << ' '; }
+	SetConsoleTextAttribute(h, GREY_BG_BLUE_TEXT);
+	ClearMain();
+	ClearSide();
+	ClearControls();
 }
-
 void DrawGrid() {
-	SetConsoleTextAttribute(h, 112);
-	print(LEFT_MAIN,	TOP_MAIN,		218);
-	print(RIGHT_MAIN,	TOP_MAIN,		194);
-	print(LEFT_MAIN,	BOTTOM_MAIN,	192);
-	print(RIGHT_MAIN,	BOTTOM_MAIN,	193);
-	print(RIGHT_SIDE,	TOP_MAIN,		191);
-	print(RIGHT_SIDE,	BOTTOM_MAIN,	217);
+	SetConsoleTextAttribute(h, GREY_BG_BLACK_TEXT);
+		print(LEFT_MAIN,		TOP_MAIN,		218);
+		print(RIGHT_MAIN,		TOP_MAIN,		194);
+		print(LEFT_MAIN,		BOTTOM_MAIN,	192);
+		print(RIGHT_MAIN,		BOTTOM_MAIN,	193);
+		print(RIGHT_SIDE,		TOP_MAIN,		191);
+		print(RIGHT_SIDE,		BOTTOM_MAIN,	217);
 
 	for (int i = 1; i < RIGHT_MAIN; i++) {
-		print(i, TOP_MAIN,		196);
-		print(i, BOTTOM_MAIN,	196);
+		print(i,				TOP_MAIN,		196);
+		print(i,				BOTTOM_MAIN,	196);
 	}
-	for (int i = 1; i < 37; i++)
+	for (int i = 1; i < RIGHT_SIDE - RIGHT_MAIN; i++)
 	{
-		print(i + RIGHT_MAIN, TOP_MAIN,		196);
-		print(i + RIGHT_MAIN, BOTTOM_MAIN,	196);
+		print(i + RIGHT_MAIN,	TOP_MAIN,		196);
+		print(i + RIGHT_MAIN,	BOTTOM_MAIN,	196);
 	}
-	for (int i = 2; i < 26; i++){
-		print(LEFT_MAIN,	i,	179);
-		print(RIGHT_MAIN,	i,	179);
-		print(RIGHT_SIDE,	i,	179);
+	for (int i = 2; i < BOTTOM_MAIN; i++){
+		print(LEFT_MAIN,		i,				179);
+		print(RIGHT_MAIN,		i,				179);
+		print(RIGHT_SIDE,		i,				179);
 	}
 }
+void DrawControls(string key, string description) {
+	SetConsoleTextAttribute(h, CYAN_BG_WHITE_TEXT);
+	cout << key << "\t";
+	SetConsoleTextAttribute(h, CYAN_BG_BLACK_TEXT);
+	cout << description;
+}
+void UpdateControls() {
+	Goto(130 / 4 * 0, TOP_CONTROLS_IN);
+	DrawControls("   F1", "Hepl");
+	Goto(130 / 4 * 0, TOP_CONTROLS_IN + 1);
+	DrawControls("   ESC", "Exit");
+
+	Goto(130 / 4 * 1, TOP_CONTROLS_IN);
+	DrawControls("UP/DOWN  ", "Select Item");
+	Goto(130 / 4 * 1, TOP_CONTROLS_IN + 1);
+	DrawControls("LEFT/RIGHT", "Exit");
+
+	Goto(130 / 4 * 2, TOP_CONTROLS_IN);
+	DrawControls("+/-", "Change Values");
+	Goto(130 / 4 * 2, TOP_CONTROLS_IN + 1);
+	DrawControls("Enter", "Select > Sub Menu");
+
+	Goto(130 / 4 * 3, TOP_CONTROLS_IN);
+	DrawControls("F9", "Setup Defaults");
+	Goto(130 / 4 * 3, TOP_CONTROLS_IN + 1);
+	DrawControls("F10", "Save and Exit");
+}
+
+deque<Menu> menues;
 
 void UpdateHeadMenu() {
-	cout << '\n';
-	SetConsoleTextAttribute(h, 121);
+	SetConsoleTextAttribute(h, GREY_BG_BLACK_TEXT);
 	int n = 6;
 	for (int i = 0; i < n; i++) {
 		menues[i].Setup(i * 15);
@@ -188,77 +229,77 @@ void UpdateHeadMenu() {
 
 int main()
 {	
-	//Needed serious refactoring!!!
 	system("chcp 437");
 	system("cls");
 	DrawBG();
 
-	Menu m1 = Menu("Main");
-	Menu m2 = Menu("Advanced");
-	Menu m25 = Menu("Security");
-	Menu m3 = Menu("Power");
-	Menu m4 = Menu("Boot");
-	Menu m5 = Menu("Exit");
+	menues.push_back(Menu("Main"));
+	menues.push_back(Menu("Advanced"));
+	menues.push_back(Menu("Security"));
+	menues.push_back(Menu("Power"));
+	menues.push_back(Menu("Boot"));
+	menues.push_back(Menu("Exit"));
 
-	m1.selected = true;
+	menues[0].selected = true;
 
-	Item items[5] = {Item("System Time", 0), Item("System Date",1), Item("Legacy Diskette A",2), Item("Legacy Diskette B", 3), Item("Floppy 3 Mode Support", 4)};
-	m1.items = items;
-	m1.items_length = 5;
+	menues[0].items.push_back(Item("System Time", 0));
+	menues[0].items.push_back(Item("System Date", 1));
+	menues[0].items.push_back(Item("Legacy Diskette A", 2));
+	menues[0].items.push_back(Item("Legacy Diskette B", 3));
+	menues[0].items.push_back(Item("Floppy 3 Mode Support", 4));
 
-	Item items2[5] = { Item("CPU Speed", 0), Item("CPU: System Frequency Multiple",1), Item("System/PCI Frequency (Mhz)",2), Item("System /SDRAM Frequency Ratio", 3), Item("Load Performance Setting", 4) };
-	m2.items = items2;
-	m2.items_length = 5;
-
-	Item items3[5] = { Item("Power Management", 0), Item("Video Off Option",1), Item("Video Off Method",2), Item("HDD Power Down", 3), Item("Suspend-RAM Capability", 4) };
-	m3.items = items3;
-	m3.items_length = 5;
-
-	Item items4[4] = { Item("1. IDE Hard Drive", 0), Item("2. ATAPI CD-ROM",1), Item("3. Removable Device",2), Item("4. Other Boot Device", 3) };
-	m4.items = items4;
-	m4.items_length = 4;
-
-	Item items5[5] = { Item("Exit Saving Changes", 0), Item("Exit Discarding Changes",1), Item("Load Setup Defaults",2), Item("Discard Changes", 3), Item("Save Changes", 4)};
-	m5.items = items5;
-	m5.items_length = 5;
-
-	menues[0] = m1;
-	menues[1] = m2;
-	menues[2] = m25;
-	menues[3] = m3;
-	menues[4] = m4;
-	menues[5] = m5;
+	menues[1].items.push_back(Item("CPU Speed", 0));
+	menues[1].items.push_back(Item("CPU: System Frequency Multiple", 1));
+	menues[1].items.push_back(Item("System/PCI Frequency (Mhz)", 2));
+	menues[1].items.push_back(Item("System /SDRAM Frequency Ratio", 3));
+	menues[1].items.push_back(Item("Load Performance Setting", 4));
 	
-	UpdateHeadMenu();
+	menues[3].items.push_back(Item("Power Management", 0));
+	menues[3].items.push_back(Item("Video Off Option", 1));
+	menues[3].items.push_back(Item("Video Off Method", 2));
+	menues[3].items.push_back(Item("HDD Power Down", 3));
+	menues[3].items.push_back(Item("Suspend-RAM Capability", 4));
 
+	menues[4].items.push_back(Item("1. IDE Hard Drive", 0));
+	menues[4].items.push_back(Item("2. ATAPI CD-ROM", 1));
+	menues[4].items.push_back(Item("3. Removable Device", 2));
+	menues[4].items.push_back(Item("4. Other Boot Device", 3));
+	
+	menues[5].items.push_back(Item("Exit Saving Changes", 0));
+	menues[5].items.push_back(Item("Exit Discarding Changes", 1));
+	menues[5].items.push_back(Item("Load Setup Defaults", 2));
+	menues[5].items.push_back(Item("Discard Changes", 3));
+	menues[5].items.push_back(Item("Save Changes", 4));
+		
+	UpdateHeadMenu();
+	UpdateControls();
+	DrawGrid();
 	int m = 0;
 	menues[0].Show();
-	Goto(menues[0].x, 0);
+	Goto(menues[0].name.length(), 0);
 
 	while (true) {
 		
 		int keycode = _getch();
-		if (keycode == 77) //right arrow
+		if (keycode == RIGHT_ARROW)
 		{
 			if (m <= 4)
 			{
 				m++;
 				menues[m - 1].selected = false;
 				menues[m].selected = true;
-				Clear();
+				ClearMain();
 				menues[m].Show();
-				Goto(m * 15 + menues[m].x, 0);
 			}
 		}
-		else if (keycode == 75) { //left arrow
+		else if (keycode == LEFT_ARROW) {
 			if (m >= 1)
 			{
 				m--;
 				menues[m + 1].selected = false;
 				menues[m].selected = true;
-				Clear();
+				ClearMain();
 				menues[m].Show();
-				Goto(m * 15 + menues[m].x, 0);
 			}
 		}
 		UpdateHeadMenu();
